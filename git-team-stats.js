fc41f18106345be43f554d1405286d1d8486d7df
Tianyou Li <tianyou.li@intel.com>
2019-03-06 14:42:17 +0800

    update code

--- git-team-stats.js
+++ git-team-stats.js
@@ -1,11 +1,15 @@
-var fs   = require('fs');
-var git  = require('nodegit');
-var log  = require('gitlog');
-var date = require('date-and-time');
+const fs   = require('fs');
+const d3   = require('d3');
+const git  = require('nodegit');
+const log  = require('gitlog');
+const date = require('date-and-time');
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+const CanvasModule = require('canvas');
 
 var config = require('./config.json');
 
-var cloneActions = Object.keys(config.repos).map(
+let cloneActions = Object.keys(config.repos).map(
 		repo => {
 				return new Promise(resolve => {
 						var dir = repo;
@@ -18,7 +22,7 @@ var cloneActions = Object.keys(config.repos).map(
 		}
 );
 
-var findCommitsByAuthor = (repo, author) => {
+let findCommitsByAuthor = (repo, author) => {
 		return new Promise(resolve => {
 				const options = { repo: repo
 													, number: 2000000000
@@ -37,6 +41,44 @@ var findCommitsByAuthor = (repo, author) => {
 						resolve({author, commits});
 				});
 		});
+};
+
+let getReposAuthorsCommitsSummary = reposAuthorsCommits => {
+		
+		authorCommitsSummary = {};
+		
+		for (let repoAuthorsCommits of reposAuthorsCommits) {
+				authorCommitsSummary[repoAuthorsCommits.repo] = {};
+				authorCommitsSummary[repoAuthorsCommits.repo].commits = [];
+				authorCommitsSummary[repoAuthorsCommits.repo].total = 0;
+				
+				for (let authorCommits of repoAuthorsCommits.authorsCommits) {
+						authorCommits.total = authorCommits.commits.length;
+						authorCommitsSummary[repoAuthorsCommits.repo].total += authorCommits.total;
+						for (let commit of authorCommits.commits) {
+								let time = date.parse(commit.authorDate.substr(0,19), 'YYYY-MM-DD HH:mm:ss');
+								if (authorCommits.timeline == null) {
+										authorCommits.timeline = {};
+								}
+								if (authorCommits.timeline[time.getFullYear()] == null) {
+										authorCommits.timeline[time.getFullYear()] = {};
+										authorCommits.timeline[time.getFullYear()].total = 0;
+								}								
+								if (authorCommits.timeline[time.getFullYear()][time.getMonth()] == null) {
+										authorCommits.timeline[time.getFullYear()][time.getMonth()] = 0;
+								}
+
+								authorCommits.timeline[time.getFullYear()][time.getMonth()] +=1;
+								authorCommits.timeline[time.getFullYear()].total +=1;
+						}
+
+						delete authorCommits.commits;
+
+						authorCommitsSummary[repoAuthorsCommits.repo].commits.push(authorCommits);
+				}
+		}
+
+		return authorCommitsSummary;
 };
 
 Promise.all(cloneActions).then(async repos => {
@@ -58,38 +100,18 @@ Promise.all(cloneActions).then(async repos => {
 
 		return reposAuthorsCommits;
 }).then(reposAuthorsCommits => {
-		authorCommitsSummary = {};
-		
-		for (let repoAuthorsCommits of reposAuthorsCommits) {
-				authorCommitsSummary[repoAuthorsCommits.repo] = [];
-				
-				for (let authorCommits of repoAuthorsCommits.authorsCommits) {
-						authorCommits.total = authorCommits.commits.length;
-
-						for (let commit of authorCommits.commits) {
-								var time = date.parse(commit.authorDate.substr(0,19), 'YYYY-MM-DD HH:mm:ss');
-								if (authorCommits.timeline == null) {
-										authorCommits.timeline = {};
-								}
-								if (authorCommits.timeline[time.getFullYear()] == null) {
-										authorCommits.timeline[time.getFullYear()] = {};
-								}								
-								if (authorCommits.timeline[time.getFullYear()][time.getMonth()] == null) {
-										authorCommits.timeline[time.getFullYear()][time.getMonth()] = 0;
-								}
-
-								authorCommits.timeline[time.getFullYear()][time.getMonth()] +=1;
-						}
-
-						delete authorCommits.commits;
-
-						authorCommitsSummary[repoAuthorsCommits.repo].push(authorCommits);
-				}
-		}
-
-		return authorCommitsSummary;
+		return getReposAuthorsCommitsSummary(reposAuthorsCommits);
 }).then(authorCommitsSummary => {
-		console.log(authorCommitsSummary);
+		let canvas  = new CanvasModule.createCanvas(1000, 500);
+		let context = canvas.getContext("2d");
+		let dom = new JSDOM();
+		
+		global.document = dom.window.document;
+		
+		let svg = d3.create("svg");
+
+		canvas.createPNGStream().pipe(fs.createWriteStream("./canvas.png"));
+		console.log(JSON.stringify(authorCommitsSummary,null, " "));
 });
 
 
